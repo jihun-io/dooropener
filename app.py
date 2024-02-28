eab0aece12dbfe6513b19b5cf60ad74b2f066dc2
김지훈 <lunarr@me.com>
2024-02-29 01:04:44 +0900

    토큰으로 열기 주소 오류 수정

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ def generate_token():
                 conn.close()
 
                 domain = request.host_url
-                scLink = domain + "/sc?t=" + token
+                scLink = domain + "sc?t=" + token
                 return render_template('generate_result.html', token=token, scLink=scLink)
             else:
                 return redirect(url_for('index'))
