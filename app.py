24cf623bc5b6823288aaa2ea103382a4567aa950
김지훈 <kim@jihun.io>
2024-03-04 02:01:19 +0900

    app 연동 작업
    1. /openwithapp으로 열기 추가
    2. /useragenttest 추가

--- app.py
+++ app.py
@@ -99,6 +99,18 @@ def open():
         return render_template('open.html', username=session['user_username'], message="문을 여는 중...")
     else:
         return redirect(url_for('index'))
+    
+@app.route('/openwithapp')
+def openwithapp():    
+    if 'user_id' in session:
+        subprocess.run(['python3', 'controller.py'])
+        return render_template('openwithapp.html', message="문을 열었습니다.")
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/useragenttest')
+def useragenttest():    
+    return render_template('openwithapp.html', message=request.user_agent.string)
 
 @app.route('/success', methods=['GET', 'POST'])
 def success():    
