f82bb61e25ae42c76f0fd22cd41deb8d9fa275b3
김지훈 <kim@jihun.io>
2024-05-12 15:06:32 +0900

    Update fcm_push.py

--- fcm_push.py
+++ fcm_push.py
@@ -1,5 +1,5 @@
 import firebase_admin
-from firebase_admin import credentials, messaging
+from firebase_admin import credentials, messaging, exceptions
 
 cred_path = "dooropener-io-jihun-firebase-adminsdk-k3aex-9f80f3588d.json"
 cred = credentials.Certificate(cred_path)
@@ -24,5 +24,5 @@ def send(token, header, body):
     try:
         response = messaging.send(message)
         print('Successfully sent message:', response)
-    except messaging.ApiCallError as e:
+    except exceptions.FirebaseError as e:
         print(f'Failed to send message to {token}: {e}')
