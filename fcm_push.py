b8400ef2adb4b981f320c74462f208a36047deca
김지훈 <kim@jihun.io>
2024-05-12 10:47:34 +0900

    채널 ID 등록

--- fcm_push.py
+++ fcm_push.py
@@ -6,16 +6,20 @@ cred_path = "dooropener-io-jihun-firebase-adminsdk-k3aex-9f80f3588d.json"
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
-
 def send(token, header, body):
     registration_token = token
 
+    # Android 알림 설정
+    android_notification = messaging.AndroidNotification(channel_id="500")
+    android_config = messaging.AndroidConfig(notification=android_notification)
+
     message = messaging.Message(
-        notification = messaging.Notification(
+        notification=messaging.Notification(
             title=header,
             body=body
         ),
         token=registration_token,
+        android=android_config,  # Android 설정 추가
     )
 
     response = messaging.send(message)
